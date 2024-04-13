a62a4f2ded68c6de497e24bababea041505c8b09
yelo-o <flyordig@gmail.com>
2024-04-13 12:50:47 +0900

    데이터 조회, 데이터 수정, 데이터 삭제 Repository Test

--- src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -1,11 +1,16 @@
 package org.zerock.mallapi.repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.mallapi.domain.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -27,5 +32,51 @@ public class TodoRepositoryTests {
 
             todoRepository.save(todo);
         }
+    }
+
+    @Test
+    public void testRead() {
+        // 존재하는 번호로 확인
+        Long tno = 33L;
+
+        java.util.Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo = result.orElseThrow();
+
+        log.info(todo);
+    }
+
+    @Test
+    public void testModify() {
+        Long tno = 34L;
+
+        Optional<Todo> result = todoRepository.findById(tno); // java.util 패키지의 Optional
+
+        Todo todo = result.orElseThrow();
+        todo.changeTitle("Modified 34...");
+        todo.changeComplete(true);
+        todo.changeDueDate(LocalDate.of(2024,04,13));
+
+        todoRepository.save(todo);
+        
+    }
+
+    @Test
+    public void testDelete() {
+        Long tno = 1L;
+
+        todoRepository.deleteById(tno);
+    }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = 
+            PageRequest.of(0, 10, Sort.by("tno").descending());
+
+        Page<Todo> result = todoRepository.findAll(pageable);
+
+        log.info(result.getTotalElements());
+
+        result.getContent().stream().forEach(todo -> log.info(todo));
     }
 }
