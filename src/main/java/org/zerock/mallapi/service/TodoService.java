dd2d22fd1c1c027c8413a75feef4f066032a7108
yelo-o <flyordig@gmail.com>
2024-04-13 12:55:06 +0900

    조회, 수정 삭제 Todo Repository

--- src/main/java/org/zerock/mallapi/service/TodoService.java
+++ src/main/java/org/zerock/mallapi/service/TodoService.java
@@ -4,4 +4,10 @@ import org.zerock.mallapi.dto.TodoDTO;
 
 public interface TodoService {
     Long register(TodoDTO todoDTO);
+
+    TodoDTO get(Long tno);
+
+    void modify(TodoDTO todoDTO);
+
+    void remove(Long tno);
 }
