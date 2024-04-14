4ef3e752feab13c1c153c0ea250b0581cb0bd193
yelo-o <flyordig@gmail.com>
2024-04-15 00:32:27 +0900

    post 방식의 등록 처리

--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -9,17 +9,29 @@ import org.zerock.mallapi.service.TodoService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
-@RequestMapping("api/todo")
+@RequestMapping("/api/todo")
 public class TodoController {
 
     private final TodoService service;
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO : " + todoDTO);
+        Long tno = service.register(todoDTO);
+        return Map.of("TNO", tno);
+    }
 
     @GetMapping("/{tno}")
     public TodoDTO get(@PathVariable(name = "tno") Long tno) {
