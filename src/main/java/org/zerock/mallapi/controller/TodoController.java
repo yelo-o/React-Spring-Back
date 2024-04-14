5e0c34d3ca207fd2c96ec93e0e60bfbc5154484b
yelo-o <flyordig@gmail.com>
2024-04-14 23:08:16 +0900

    controller 추가 - get

---
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -0,0 +1,34 @@
+package org.zerock.mallapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.mallapi.dto.PageRequestDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
+import org.zerock.mallapi.dto.TodoDTO;
+import org.zerock.mallapi.service.TodoService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("api/todo")
+public class TodoController {
+
+    private final TodoService service;
+
+    @GetMapping("/{tno}")
+    public TodoDTO get(@PathVariable(name = "tno") Long tno) {
+        return service.get(tno);
+    }
+
+    @GetMapping("/list")
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
+        log.info(pageRequestDTO);
+        return service.list(pageRequestDTO);
+    }
+}
