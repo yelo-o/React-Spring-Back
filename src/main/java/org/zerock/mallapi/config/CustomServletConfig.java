44cc3978f463310f53537541d9c42e99989d4272
yelo-o <flyordig@gmail.com>
2024-04-15 00:00:13 +0900

    LocalDateFormatter 추가 및 config 패키지에 추가

---
+++ src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -0,0 +1,16 @@
+package org.zerock.mallapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
+    }
+    
+}
